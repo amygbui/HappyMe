8f9de469e7b0462cdb15631056736a1ee1cade64
Amy Bui <amygbui@gmail.com>
2017-04-18 19:50:22 -0400

    Finish header styling

--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -17,27 +17,34 @@ class Greeting extends React.Component {
 
     if (this.props.currentUser) {
       header = (
-        <div>
-          <h2>Hello, {this.props.currentUser.name}!</h2>
+        <span className="session-link">
           <button onClick={this.logout}>Log Out</button>
-        </div>
+        </span>
       );
     } else {
       header = (
         <nav className="session-link">
           <Link to='signup'>Sign Up</Link>
-          <Link to='login'>Log In</Link>
         </nav>
       );
     }
 
     return(
-      <div className="greeting">
-        <header className="header">
+      <div className="header">
+        <header className="header-fixed">
           <Link to='/'><h1>HappyMe</h1></Link>
           { header }
         </header>
-        <img src={ window.images.backsplash } />
+
+        <nav className="nav">
+          <nav className="feature-pages">
+            <Link to='/'>Beers</Link>
+            <Link to='/'>Cocktails</Link>
+            <Link to='/'>Oysters</Link>
+            <Link to='/'>Food</Link>
+          </nav>
+          <Link to='login'>Log In</Link>
+        </nav>
       </div>
     )
   }
