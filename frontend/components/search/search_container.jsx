f9796114d0b03195a16791bde39e10e3680e3109
Amy Bui <amygbui@gmail.com>
2017-04-19 23:22:30 -0400

    Import SessionApiUtil to Search component (need to add actions & reducer)

--- frontend/components/search/search_container.jsx
+++ frontend/components/search/search_container.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+import { fetchRestaurants } from '../../util/search_api_util'
 import Search from './search';
 
 const mapDispatchToProps = dispatch => {
