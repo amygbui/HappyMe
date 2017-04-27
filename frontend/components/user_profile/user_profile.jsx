88a3ead6150b459cd3ac3c19a76de7dc68206de9
Amy Bui <amygbui@gmail.com>
2017-04-26 23:58:43 -0400

    Start adding content to and styling user's profile page

--- frontend/components/user_profile/user_profile.jsx
+++ frontend/components/user_profile/user_profile.jsx
@@ -1,10 +1,6 @@
 import React from 'react';
 
 class UserProfile extends React.Component {
-  constructor(props) {
-    super(props)
-  }
-
   componentDidMount() {
     const userIds = Object.keys(this.props.users);
     if (!userIds.includes(this.props.params.userId)) {
@@ -20,8 +16,37 @@ class UserProfile extends React.Component {
   }
 
   render() {
+    if (!this.props.user) {
+      return(<div>Loading...</div>)
+    }
+    const { name, image_url, reviews } = this.props.user
+    const totalReviews = Object.keys(reviews).length
+
     return(
-      <div>HELLO FINE USER</div>
+      <div>
+        <main className="userHeader">
+          <section className="userStats">
+            <article>
+              <img src={ image_url } />
+              <p>
+                <h2>{ name }</h2>
+                <i className="fa fa-newspaper-o" aria-hidden="true"></i>
+                { totalReviews } reviews
+              </p>
+            </article>
+            <ul>
+              <li>
+                <i className="fa fa-camera" aria-hidden="true"></i>
+                Photos
+              </li>
+              <li>
+                <i className="fa fa-user-circle-o" aria-hidden="true"></i>
+                Friends
+              </li>
+            </ul>
+          </section>
+        </main>
+      </div>
     )
   }
 }
