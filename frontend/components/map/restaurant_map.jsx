8773d4cb8319b62b90b2ca0d12e9fae0dce4a256
Amy Bui <amygbui@gmail.com>
2017-05-06 19:51:47 -0400

    Fix map issues

--- frontend/components/map/restaurant_map.jsx
+++ frontend/components/map/restaurant_map.jsx
@@ -5,22 +5,41 @@ import MarkerManager from '../../util/marker_manager';
 class RestaurantMap extends React.Component {
   constructor(props) {
     super(props)
+    this.calculateCenter = this.calculateCenter.bind(this);
+  }
+
+  calculateCenter() {
+    const { northeast, southwest } = this.props.bounds;
+    const lat = (northeast.lat + southwest.lat) / 2;
+    const lng = (northeast.lng + southwest.lng) / 2;
+
+    return { lat, lng }
   }
 
   componentDidMount() {
     const mapOptions = {
-      center: { lat: 40.743817, lng: -73.994521 },
+      center: this.calculateCenter(),
       zoom: 11
     };
 
     this.map = new google.maps.Map(this.mapNode, mapOptions);
     this.MarkerManager = new MarkerManager(this.map);
-    this.MarkerManager.updateMarkers(Object.values(this.props.restaurants));
+
+    if (Object.values(this.props.restaurants).length > 0) {
+      this.MarkerManager.updateMarkers(Object.values(this.props.restaurants));
+    }
   }
 
   componentWillReceiveProps(nextProps) {
     if (this.props.restaurants != nextProps.restaurants) {
-      this.MarkerManager.updateMarkers(Object.values(nextProps.restaurants));
+      this.map.setCenter(this.calculateCenter());
+      this.map.setZoom(11);
+
+      if (Object.values(nextProps.restaurants).length > 0) {
+        this.MarkerManager.updateMarkers(Object.values(nextProps.restaurants));
+      } else {
+        this.MarkerManager.deleteMarkers();
+      }
     }
   }
 
